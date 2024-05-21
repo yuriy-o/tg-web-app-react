7a428a7c83eb7cd5750f6f82ab7dd9ead602b497
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2024-05-21 20:14:13 +0300

    adds custom hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import { useEffect } from 'react';
+import { useTelegram } from './hooks/useTelegram';
 
 function App() {
+    const { tg, onToggleButton } = useTelegram();
+
     useEffect(() => {
         tg.ready();
     }, []);
 
     return (
         <div className="App">
-            Hello world !<button onClick={onClose}>Закрити</button>
+            Hello world !<button onClick={onToggleButton}>Toggle</button>
         </div>
     );
 }
