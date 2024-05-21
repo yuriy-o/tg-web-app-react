7df3e099ac1f451ea1d32d2d7a5b0d853f5b2186
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2024-05-21 20:26:58 +0300

    deletes mistake

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
     useEffect(() => {
         tg.ready();
-    }, []);
+    }, [tg]);
 
     return (
         <div className="App">
--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -9,7 +9,7 @@ export function useTelegram() {
         if (tg.MainButton.isVisible) {
             tg.MainButton.hide();
         } else {
-            tg.MainButton.close();
+            tg.MainButton.show();
         }
     };
 
