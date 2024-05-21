e30559f5f331339475027df526f0634e2ddec3c4
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2024-05-22 01:48:53 +0300

    adds <Form />

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import './App.css';
 import { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
 import { useTelegram } from './hooks/useTelegram';
 import Header from './components/Header/Header';
+import './App.css';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
 
 function App() {
     const { tg, onToggleButton } = useTelegram();
@@ -13,7 +17,10 @@ function App() {
     return (
         <div className="App">
             <Header />
-
+            <Routes>
+                <Route index element={<ProductList />} />
+                <Route path={'form'} element={<Form />} />
+            </Routes>
             <button onClick={onToggleButton}>Toggle</button>
         </div>
     );
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import './Form.css';
+import { useTelegram } from '../../hooks/useTelegram';
+
+const Form = () => {
+    const [country, setCountry] = useState('');
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
+    const { tg } = useTelegram();
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Відправити дані',
+        });
+    }, []);
+
+    useEffect(() => {
+        if (!country || !street) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [country, street]);
+
+    const onChangeCountry = e => {
+        setCountry(e.target.value);
+    };
+    const onChangeStreet = e => {
+        setStreet(e.target.value);
+    };
+    const onChangeSubject = e => {
+        setSubject(e.target.value);
+    };
+
+    return (
+        <div className={'form'}>
+            <h3>Введіть ваші дані</h3>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Країна'}
+                value={country}
+                onChange={onChangeCountry}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Вулиця'}
+                value={street}
+                onChange={onChangeStreet}
+            />
+
+            <select
+                className={'select'}
+                value={subject}
+                onChange={onChangeSubject}
+            >
+                <option value={'physical'}>ФОП</option>
+                <option value={'legal'}>Юр. лице</option>
+            </select>
+        </div>
+    );
+};
+
+export default Form;
