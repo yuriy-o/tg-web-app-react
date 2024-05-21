05de8ae69bc8c52de26331c581c2318ec65a5a6c
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2024-05-21 19:41:47 +0300

    adds styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
 import { useEffect } from 'react';
 
-const tg = window.Telegram.WebApp;
-
 function App() {
     useEffect(() => {
         tg.ready();
     }, []);
-
-    const onClose = () => {
-        tg.close();
-    };
 
     return (
         <div className="App">
