3ab21ed4932605c06862fda6c30fdeec1393861b
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2024-05-22 13:53:16 +0300

    adds sendData(data)

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './Form.css';
 import { useTelegram } from '../../hooks/useTelegram';
 
@@ -7,6 +7,24 @@ const Form = () => {
     const [street, setStreet] = useState('');
     const [subject, setSubject] = useState('physical');
     const { tg } = useTelegram();
+
+    const onSendData = useCallback(() => {
+        const data = {
+            country,
+            street,
+            subject,
+        };
+
+        tg.sendData(JSON.stringify(data));
+    }, []);
+
+    useEffect(() => {
+        tg.onEvent('backButtonClicked', onSendData);
+
+        return () => {
+            tg.offEvent('backButtonClicked', onSendData);
+        };
+    }, []);
 
     useEffect(() => {
         tg.MainButton.setParams({
